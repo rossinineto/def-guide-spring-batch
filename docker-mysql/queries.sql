c3f6f3e2ca3136169904f2800b96c42167e42427
rossini.neto <rossini.neto@gmail.com>
2022-08-05 00:00:00 -0300

    DMLs para manipulação dos metadatas e outros dados dos batchs

---
+++ docker-mysql/queries.sql
@@ -0,0 +1,16 @@
+select table_schema as database_name,
+    table_name
+from information_schema.tables
+where table_type = 'BASE TABLE'
+        and table_schema = 'spring_batch' -- enter your database name here
+order by database_name, table_name;
+
+delete from spring_batch.BATCH_STEP_EXECUTION_SEQ;
+delete from spring_batch.BATCH_STEP_EXECUTION_CONTEXT;
+delete from spring_batch.BATCH_STEP_EXECUTION;
+delete from spring_batch.BATCH_JOB_SEQ;
+delete from spring_batch.BATCH_JOB_EXECUTION_SEQ;
+delete from spring_batch.BATCH_JOB_EXECUTION_CONTEXT;
+delete from spring_batch.BATCH_JOB_EXECUTION_PARAMS;
+delete from spring_batch.BATCH_JOB_EXECUTION;
+delete from spring_batch.BATCH_JOB_INSTANCE;
